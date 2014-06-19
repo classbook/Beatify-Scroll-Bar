2154e9e6b1df449ada2d414c9f761c672ec3a66b
Kishore <yoursbalakishore@gmail.com>
2014-06-20 03:07:23 +0530

    Make project layout.

---
+++ js/beautify.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+(function($){
+	var settings = $.extend({
+		autoHide	:	true,
+		ScrollBarCss:	"scrollable"
+	}, options);
+
+	$.fn.beautifyScrollBar = function(){
+		return this.each(function(){
+			/*Beautify CSS Functinality goes here!!!*/
+		});
+	};
+}(jQuery));
